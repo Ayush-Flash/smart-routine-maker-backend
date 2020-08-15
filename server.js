9c9444587f3c64154393071dd629441bb1c58519
Ayush-Flash <Ayush-Flash>
2020-08-15 23:52:57 +0530

    FIX

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ server.use((req, res, next) => {
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
         return res.status(200).json({});
     }
+    next();
 })
 
 server.use(cors());
