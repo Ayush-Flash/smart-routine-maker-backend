e192b81245c37777cf6894dac053df53e33bc5ee
Ayush-Flash <Ayush-Flash>
2020-08-16 01:07:02 +0530

    Fixed GetROUTINES bug

--- src/routes/getRoutines.js
+++ src/routes/getRoutines.js
@@ -5,7 +5,11 @@ const router = express.Router();
 router.post('/', (req, res) => {
     UserRoutine.findOne({username : req.body.username})
                .then(user => {
-                   res.json(user.routines)
+                   if(user) {
+                       res.json(user.routines)
+                   } else {
+                       res.json(null)
+                   }
                 })
                .catch(err => console.log(err));
 })
